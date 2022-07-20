d42c51d71553b5285a5e68e1687c4d593c13a8b2
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2022-07-20 17:48:53 +0300

    minor refactoring

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,11 +1,7 @@
-from pickle import BUILD
 import numpy as np
 
-from legend       import *
-from logger       import logger
-from scipy.signal import medfilt
-
-torch = None
+from legend import *
+from logger import logger
 
 
 class LandCover:
@@ -141,104 +137,87 @@ class Shadow:
     def __call__(self, line_pair):
         """
         Cast a shadow line as if the
-        azimuth angle is 0 degrees.
+        azimuth angle is 270 degrees.
         """
-        line, bh = line_pair
-        mask = bh > 0
+        # line, bh = line_pair
+        # mask = bh > 0
         
-        # Why am I computing tan(theta) each time?
-        # Compute it once here.
-        theta = (90 - self.sun_angles['zenith']) * np.pi / 180
+        # # Why am I computing tan(theta) each time?
+        # # Compute it once here.
+        # theta = (90 - self.sun_angles['zenith']) * np.pi / 180
         
-        while mask.any():
+        # while mask.any():
             
-            # Jump to the index with value.
-            idx = torch.where(mask)[0][0]
+        #     # Jump to the index with value.
+        #     idx = torch.where(mask)[0][0]
             
-            # Get height at location.
-            height = bh[idx]
+        #     # Get height at location.
+        #     height = bh[idx]
             
-            if height < line[idx]:
-                """
-                # TODO
-                Move if location is already
-                written successfully.
+        #     if height < line[idx]:
+        #         """
+        #         # TODO
+        #         Move if location is already
+        #         written successfully.
                 
-                This needs to be improved
-                by jumping more than one location.
+        #         This needs to be improved
+        #         by jumping more than one location.
                 
-                # TODO
-                # Room for improvement.
-                """
-                jump = 1
-                line, bh = line[jump:], bh[jump:]
-                mask = bh > 0
-                continue
+        #         # TODO
+        #         # Room for improvement.
+        #         """
+        #         jump = 1
+        #         line, bh = line[jump:], bh[jump:]
+        #         mask = bh > 0
+        #         continue
                         
-            # Jump rows to location.
-            line = line[idx:]
-            bh = bh[idx:]
+        #     # Jump rows to location.
+        #     line = line[idx:]
+        #     bh = bh[idx:]
             
-            # Calculate dislocation
-            d = height / np.tan(theta)
-            d = d.round().to(int)
+        #     # Calculate dislocation
+        #     d = height / np.tan(theta)
+        #     d = d.round().to(int)
             
-            """
-            The roof ends where the height changes ahead.
-            """
-            # Check where the rooftop ends.
-            roof_top_end = torch.where(bh != height)[0]
-            roof_top_end = roof_top_end[0] if roof_top_end.any() else 0
+        #     """
+        #     The roof ends where the height changes ahead.
+        #     """
+        #     # Check where the rooftop ends.
+        #     roof_top_end = torch.where(bh != height)[0]
+        #     roof_top_end = roof_top_end[0] if roof_top_end.any() else 0
             
-            # Add rooftop length to dislocation
-            d += roof_top_end + 1
-            """
-            Check if anything is taller ahead.
-            If there is an obstacle, adjust d.
-            """
-            # Check for obstacles within dislocation range.
-            check_obstacle = torch.where(bh[:d] > height)[0]
-            # Shorten dislocation if obstacle is found.
-            d = d if not check_obstacle.any() else check_obstacle[0]
+        #     # Add rooftop length to dislocation
+        #     d += roof_top_end + 1
+        #     """
+        #     Check if anything is taller ahead.
+        #     If there is an obstacle, adjust d.
+        #     """
+        #     # Check for obstacles within dislocation range.
+        #     check_obstacle = torch.where(bh[:d] > height)[0]
+        #     # Shorten dislocation if obstacle is found.
+        #     d = d if not check_obstacle.any() else check_obstacle[0]
             
-            """
-            Adjust d incase shadow lands on higher ground (another building).
-            """
+        #     """
+        #     Adjust d incase shadow lands on higher ground (another building).
+        #     """
             
-            try:
-                new_height = bh[d]
-                if new_height < height:
-                    d2 = new_height / np.tan(theta)
-                    d2 = d2.round().to(int)
-                    d -= d2
-            except IndexError:
-                pass
+        #     try:
+        #         new_height = bh[d]
+        #         if new_height < height:
+        #             d2 = new_height / np.tan(theta)
+        #             d2 = d2.round().to(int)
+        #             d -= d2
+        #     except IndexError:
+        #         pass
             
-            # Write shadow + rooftop    
-            line[:d] = height
+        #     # Write shadow + rooftop    
+        #     line[:d] = height
             
-            # Next location should be the edge of the rooftop plus one.
-            end = roof_top_end + 1
-            line, bh = line[end:], bh[end:]
+        #     # Next location should be the edge of the rooftop plus one.
+        #     end = roof_top_end + 1
+        #     line, bh = line[end:], bh[end:]
             
-            # Get a new mask.
-            mask = bh > 0
+        #     # Get a new mask.
+        #     mask = bh > 0
             
-    def remove_footprints(self):
-        """
-        Remove the building footprints
-        from the shadow array if they are
-        taller than the shadows.
-        """
-        idx = torch.where(self.shadows <= self.bh_array)
-        self.shadows[idx] = 0
-        
-    def binarize_shadows(self):
-        """
-        Binarize the shadows array,
-        and remove padding.
-        """
-        self.shadows[self.shadows > 0] = 1
-        self.shadows = torch.from_numpy(self.shadows)
-        self.shadows = self.unpad(self.shadows).numpy()
     
