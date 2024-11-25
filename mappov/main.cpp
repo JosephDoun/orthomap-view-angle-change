e6c5c3f266b4e5ccc80673332000fe4d0fd9e25f
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-11-25 23:05:40 +0100

    refactor: added main func encapsulation for exceptions

--- mappov/main.cpp
+++ mappov/main.cpp
@@ -1,26 +1,18 @@
-#include "version.h"
-#include "filesystem/read.h"
-#include "config/args.h"
-
-#include <cstdio>
+#include "__main_app.cpp"
 
 
 // Entry point.
 int main(int argc, const char * argv[])
 {	
-	
-	Args cliargs{argc, argv};
-
-	// Instantiate reader.
-	DatasetReader reader;
-
-	GDALDataset * map = reader.read(cliargs.lcmap);
-	GDALDataset * dem = reader.read(cliargs.dem);
-	
-	printf("%d.%d.%d\n",
-			__MAPPOV_VERSION_MAJOR,
-			__MAPPOV_VERSION_MINOR,
-			__MAPPOV_VERSION_PATCH);
+	try
+	{
+		return __main(argc, argv);
+	}
+	catch (const std::exception &e)
+	{
+		printf("%s\n", e.what());
+		return 1;
+	}
 
 	return 0;
 }
