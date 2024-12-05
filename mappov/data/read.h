810e35e9061e762218c60fc85d984aeeca1b71ac
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-12-05 17:50:20 +0100

    feat: redesigned dataset class;

--- mappov/data/read.h
+++ mappov/data/read.h
@@ -1,27 +1,38 @@
-# ifndef __READ
-# define __READ
+# ifndef __READ_DATA
+# define __READ_DATA
+
 # include <gdal/gdal_priv.h>
+# include "../memory/memory.h"
 
 
 struct _t_coords;
 typedef _t_coords t_coords;
 
 
-struct Dataset: public GDALDataset
+struct Dataset
 {
 	private:
-	uint16_t t_size;
-	uint16_t n_tiles;
+	/* GDALDataset */
+	GDALDatasetUniquePtr ds;
+	Memory mem;
+	/* fetch-tile dimensions. */
+	int t_size_x;
+	int t_size_y;
+	/* Number of tiles in dataset. */
+	int n_tiles;
+	/* Function returning tile coordinates in pixels. */
 	t_coords tile_coords(uint16_t);
 
 	public:
-	/* Tile size setter function. */
-	Dataset * SetTSize(uint16_t);
+	Dataset(std::string);
+	Dataset(std::string, uint16_t, uint16_t);
+	
 	float * operator[](uint16_t);
 
 	/* Read a raster from file into a dataset. */
-	static Dataset * ReadDataset(std::string /* File path. */ p);
+	static GDALDatasetUniquePtr ReadDataset(std::string /* File path. */ p);
 
+	/* Friend functions. */
 	friend void Transform(Dataset*, Dataset*, float, float);
 
 };
